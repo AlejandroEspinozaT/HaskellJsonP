669433af6b173a48dad6776a3d960d045069d214
Alejandro Espinoza <alejandro.espinoza.60092@usalesiana.edu.bo>
2023-04-26 19:36:33 -0400

    all parsers functions implemented

--- src/avance/HaskellJsonLib.hs
+++ src/avance/HaskellJsonLib.hs
@@ -1,6 +1,9 @@
+module HaskellJsonLib where
+
 import Control.Applicative (Applicative(pure, (<*>)), Alternative (empty, (<|>)))
 import Text.Read
 import Data.Char
+import Data.List
 
 data JsonValue = JString String
           | JBool Bool
@@ -17,8 +20,18 @@ writeJson (Just (JString s)) = s
 writeJson (Just (JBool b)) = show b
 writeJson (Just (JNumber n)) = show n
 writeJson (Just (JList l)) = show (map writeJson l)
---writeJson (Just (JObject o)) = "{" ++ createFromObject o ++ "}"
+writeJson (Just (JObject o)) = "{" ++ createFromObject o ++ "}"
 
+parseJson :: String -> Maybe JsonValue
+parseJson [] = Nothing
+parseJson s = Just (JObject (map buildTuple (splitAcc' ',' (extractObject' (Just s)) [] False 0)))
+
+showNode :: (String, Maybe JsonValue) -> String
+showNode (name,value) = name ++ ": " ++ writeJson value
+
+createFromObject :: [(String,Maybe JsonValue)] -> String
+createFromObject [] = ""
+createFromObject xs = intercalate "," (map showNode xs)
 
 instance Functor Parser where
     fmap f (Parser function) = Parser (\x -> case function x of
@@ -37,6 +50,14 @@ instance Applicative Parser where
 instance Alternative Parser where
     empty = Parser (\_ -> Nothing)
     (Parser a ) <|> (Parser b) = Parser (\input -> a input <|> b input)
+
+
+getJValue :: String -> Maybe JsonValue
+getJValue xs =
+  case parse (parseBool <|> parseNumber <|> parseString <|> parseList <|> parseObject) xs of
+    Just (value, _) -> Just value
+    Nothing -> Nothing
+
 
 parseBool :: Parser JsonValue
 parseBool = Parser (\x->case x of
@@ -64,56 +85,57 @@ parseList :: Parser JsonValue
 parseList = Parser $ \input ->
     if isOpenCharList (trim input)
         then case (extractObject' (Just input)) of
-            Just xs -> case traverse parseListContents (splitAcc' ',' (Just xs) [] 0 0) of
+            Just xs -> case traverse parseList' (splitAcc' ',' (Just xs) [] False 0) of
                 Just vs -> Just (JList (map Just vs), "")
                 Nothing -> Nothing
             Nothing -> Nothing
         else Nothing
         
-parseListContents :: String -> Maybe JsonValue
-parseListContents s = getJValue (trim s) >>= Just
-
+parseList' :: String -> Maybe JsonValue
+parseList' s = getJValue (trim s) 
 
 parseObject :: Parser JsonValue
 parseObject = Parser $ \input ->
-    if isOpenCharObject (trim input)
-        then case (extractObject' (Just input)) of
-                        
-            
-        else Nothing
+  if isOpenCharObject (trim input)
+    then case extractObject' (Just input)  of
+           Just objectString -> case parseObject' objectString of
+                                  Just (jobj, rest) -> Just (JObject jobj, rest)
+                                  _ -> Nothing
+           Nothing -> Nothing
+    else Nothing
 
-{-
 
-parseObject :: String -> Maybe JsonValue
-parseObject [] = Nothing
-parseObject xs = Just (JObject (parseObject' xs))
-
-parseObject' :: String -> [(String, Maybe JsonValue)]
-parseObject' [] = [("", Just JNil)]
-parseObject' xs = if isOpenCharObject xs then map buildTuple (splitAcc ',' (extractObject' (Just xs)) []) else [("",Nothing)]
--}
+parseObject' :: String -> Maybe ([(String, Maybe JsonValue)], String)
+parseObject' input = case splitAcc' ',' (Just input) [] False 0 of
+                      [] -> Just ([], "")
+                      segments -> parseJsonObj segments []
+  where
+    parseJsonObj [] acc = Just (reverse acc, "")
+    parseJsonObj (segment:segments) acc =
+      case splitAcc' ':' (Just segment) [] False 0 of
+        [key, value] -> case (trim key, getJValue (trim value)) of
+                          ("", _) -> Nothing
+                          (_, Nothing) -> Nothing
+                          (parsedKey, Just parsedValue) -> parseJsonObj segments ((parsedKey, Just parsedValue) : acc)
+        _ -> Nothing
 
 buildTuple :: String -> (String, Maybe JsonValue)
 buildTuple [] = ("", Just (JString ""))
-buildTuple s = let x : y : _ = splitAcc ':' (Just s) [] in (trim x, getJValue (trim y))
+buildTuple s = let x:y:xs = splitAcc' ':' (Just (trim s)) [] False 0 in (trim x, getJValue (trim y))
+
 
 extractObject' :: Maybe String -> Maybe String
 extractObject' Nothing = Nothing
 extractObject' (Just []) = Nothing
 extractObject' (Just (_:xs)) = Just (init xs)
 
-
 isOpenCharObject :: String -> Bool
 isOpenCharObject ('{' : _) = True
 isOpenCharObject _ = False
 
-
-getJValue :: String -> Maybe JsonValue
-getJValue xs =
-  case parse (parseBool <|> parseNumber <|> parseString <|> parseList <|> parseObject) xs of
-    Just (value, _) -> Just value
-    Nothing -> Nothing
-
+isOpenCharList :: String -> Bool
+isOpenCharList ('[' : _) = True
+isOpenCharList _ = False
 
 isDigit' :: String -> Bool
 isDigit' [] = False
@@ -121,31 +143,15 @@ isDigit' ('-' : xs) = isDigit' xs
 isDigit' [x] = isDigit x
 isDigit' (x:xs) = isDigit x && isDigit' xs
 
-isOpenCharList :: String -> Bool
-isOpenCharList ('[' : _) = True
-isOpenCharList _ = False
-
 trim :: String -> String
 trim = dropWhile isSpace
 
-splitAcc :: Char -> Maybe String -> String -> [String]
-splitAcc _ Nothing [] = []
-splitAcc _ Nothing (_ : _) = []
-splitAcc c (Just xs) ys = splitAcc' c (Just xs) ys 0 0
-
-
-splitAcc' :: Char -> Maybe String -> String -> Int -> Int -> [String]
+splitAcc' :: Char -> Maybe String -> String -> Bool -> Int -> [String]
 splitAcc' _ Nothing [] _ _ = []
 splitAcc' _ (Just []) [] _ _ = []
 splitAcc' _ (Just []) ys _ _ = [reverse ys]
-splitAcc' c (Just xs) ys bracket brace =
-    case xs of
-    x:xs' ->
-            case x of
-                ',' | bracket == 0 && brace == 0 -> reverse ys : splitAcc' c (Just xs') [] bracket brace
-                ':' | bracket == 0 && brace == 0 -> reverse ys : splitAcc' c (Just xs') [] bracket brace
-                '[' -> splitAcc' c (Just xs') (x:ys) (bracket + 1) brace
-                '{' -> splitAcc' c (Just xs') (x:ys) bracket (brace + 1)
-                ']' -> splitAcc' c (Just xs') (x:ys) (bracket - 1) brace
-                '}' -> splitAcc' c (Just xs') (x:ys) bracket (brace - 1)
-                _ -> splitAcc' c (Just xs') (x:ys) bracket brace
+splitAcc' c (Just (x:xs)) ys inBracket count
+  | x == c && not inBracket = reverse ys : splitAcc' c (Just xs) [] False count
+  | x == '[' || x == '{' = splitAcc' c (Just xs) (x:ys) True (count + 1)  
+  | x == ']' || x == '}' = splitAcc' c (Just xs) (x:ys) (count > 1) (count - 1)
+  | otherwise = splitAcc' c (Just xs) (x:ys) inBracket count
